b7c58aff18731a31bbc9c4f077a9180de5dbb713
renqingyang <rq625230882@gmail.com>
2022-05-06 18:24:59 -0500

    update

--- src/main/java/com/example/springbatchdemo/batch/demo1/CSVItemWriterListener.java
+++ src/main/java/com/example/springbatchdemo/batch/demo1/CSVItemWriterListener.java
@@ -3,11 +3,16 @@ package com.example.springbatchdemo.batch.demo1;
 import org.springframework.batch.core.ItemWriteListener;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-public class CSVItemWriterListener<T> implements ItemWriteListener<T> {
 
+public class CSVItemWriterListener<T> implements ItemWriteListener<T> {
 
     @Override
     public void beforeWrite(List<? extends T> list) {
@@ -16,7 +21,7 @@ public class CSVItemWriterListener<T> implements ItemWriteListener<T> {
 
     @Override
     public void afterWrite(List<? extends T> list) {
-        System.out.println("after write");
+        System.out.println("saved into csv file");
     }
 
     @Override
--- src/main/java/com/example/springbatchdemo/batch/demo1/SpringBatchPartitionConfig.java
+++ src/main/java/com/example/springbatchdemo/batch/demo1/SpringBatchPartitionConfig.java
@@ -2,15 +2,17 @@ package com.example.springbatchdemo.batch.demo1;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.*;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -61,8 +63,13 @@ public class SpringBatchPartitionConfig {
                 .reader(itemReader(null))
                 .processor(new CSVItemProcessor())
                 .writer(itemWriter(null))
-                .listener(new CSVItemWriterListener<>())
+                .listener(csvItemWriterListener())
                 .build();
+    }
+
+    @Bean
+    public CSVItemWriterListener csvItemWriterListener() {
+        return new CSVItemWriterListener<>();
     }
 
     @Bean
